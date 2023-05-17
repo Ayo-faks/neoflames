027b3fc2fd93f9535ccd7e92bca51a9799105edf
ayoola fakoya <ayoolafakoya@gmail.com>
2023-05-17 17:43:35 +0000

    commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
     <>
       <Breadcrumb
         pageName="About Page"
-        description="At Neoflames, we are passionate about harnessing the power of technology to create innovative solutions that make life easier for humanity. As an AI startup company, our mission is to revolutionize the way people interact with legal and health services, ultimately enhancing the overall well-being of individuals and communities.."
+        description="At NeoFlames, we are passionate about harnessing the power of technology to create innovative solutions that make life easier for humanity. As an AI startup company, our mission is to revolutionize the way people interact with legal and health services, ultimately enhancing the overall well-being of individuals and communities.."
       />
       {/* <AboutSectionOne /> */}
       <AboutSectionTwo />
